c8336fa6ddc1a14b4f2411e200e222e698ca3ad2
Nicolás Martín Rojo <nicolas.martin.rojo@gmail.com>
2026-04-01 12:57:05 -0300

    feat: initial commit

--- app/components/HouseComponent.tsx
+++ app/components/HouseComponent.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { ArrowUpIcon } from "lucide-react";
+
 export default function HouseComponent(props: House) {
   return (
     <div className="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
@@ -10,6 +13,13 @@ export default function HouseComponent(props: House) {
         <span className="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
           <span>{props.price}</span>
         </span>
+      </div>
+      <div className="flex min-h-svh items-center justify-center">
+        <Button>Click me</Button>
+        <Button variant="outline">Button</Button>
+        <Button variant="outline" size="icon" aria-label="Submit">
+          <ArrowUpIcon />
+        </Button>
       </div>
     </div>
     // <div className="rounded-sm ">
